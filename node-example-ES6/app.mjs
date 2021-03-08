3260021c3bf1ff6be68eaeca81814a986762ca38
Hermen-Nicolau <hermen.nicolau@gmail.com>
2021-03-08 10:23:52 -0500

    post workshop assignment

---
+++ node-example-ES6/app.mjs
@@ -0,0 +1,23 @@
+import rect from './rectangle.mjs';
+
+
+//const rect = require('./rectangle');
+
+function solveRect(l, w){
+    console.log(`Solving for rectangle with dimensions: ${l}, ${w}`);
+
+    rect(l, w, (err, rectangle) => {
+        if (err){
+            console.log('ERROR:', err.message);        
+        }else {
+            console.log(`Area of rectangle with dimensions ${l}, ${w}: ${rectangle.area()}`);
+            console.log(`Perimeter of rectangle with dimensions ${l}, ${w}: ${rectangle.perimeter()}`);
+        }
+    });
+    console.log("This statement is logged after te call to rect()");
+}
+
+solveRect(2, 4);
+solveRect(3, 5);
+solveRect(0, 5);
+solveRect(5, -3);
--- node-express/routes/campsiteRouter.js
+++ node-express/routes/campsiteRouter.js
@@ -21,4 +21,22 @@ campsiteRouter.route('/')
     res.end('Deleting all campsites');
 });
 
+
+campsiteRouter.route('/:campsiteId')
+.get((req, res,) => {
+    res.end('Will send all the campistes to you');
+})
+.post((req, res) => {
+    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /campsites');
+})
+.delete((req, res) => {
+    res.end('Deleting all campsites');
+});
+
+
+
 module.exports = campsiteRouter;
